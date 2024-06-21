e87eef5063ba8e53aff7ff7644026dcb3cd8162b
Jovanka <dev.kniamaps@outlook.com>
2024-06-21 12:38:06 +1000

    added css styles and edit profile button

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,34 +1,35 @@
+import ButtonPrimary from "../components/ButtonPrimary";
+
 export default function Profile() {
 
     const profileTemp = {
         name: 'Edrick Koda',
-        bio: 'idk what',
+        userType: 'Student',
+        bio: 'insert bio here or some other details',
         email: 'z12345@student.unsw.edu',
         profilePic: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/2048px-Windows_10_Default_Profile_Picture.svg.png'
     };
 
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center">
-            <div>
-                <h1>Your Profile</h1>
-            </div>
-            <div> 
+        <div className="min-h-screen items-center justify-center">
+            <h1 className="text-3xl font-semibold text-center mt-10">Your Profile</h1>
+
+            <div className="flex flex-col items-center justify-center mt-10"> 
                 <img 
                     src={profileTemp.profilePic} 
                     alt="Profile" 
-                    style={{ 
-                        width: '100px', 
-                        height: '100px', 
-                        borderRadius: '50%',
-                    }} 
+                    className="w-32 h-32 rounded-full"
                 />
+                <h2 className="text-2xl font-semibold mt-4">{profileTemp.name}</h2>
+                <h2 className="text-xl mt-2">{profileTemp.userType}</h2>
+                <p className="text-xl text-gray-600 mt-2">{profileTemp.bio}</p>
+                <h3 className="text-sm text-gray-500 mt-2">{profileTemp.email}</h3>
             </div>
-            <div>
-                <h2>{profileTemp.name}</h2>
-                <h3>{profileTemp.bio}</h3>
-                <h3>{profileTemp.email}</h3>
+
+            <div className="mt-8 w-80 mx-auto" title="Edit Profile Button">
+                <ButtonPrimary text="Edit Profile" url=""/>
             </div>
-        
+
         </div>
     )
 }
