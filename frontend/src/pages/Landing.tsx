6cd1c46e3ab206be2ad3922e64164c57be124945
Maximilian Falco Widjaya <maximilianfalco@gmail.com>
2024-06-25 16:00:10 +1000

    removed unrequired import

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -7,7 +7,6 @@ import { useForm } from "react-hook-form";
 import { useNavigate, Link } from "react-router-dom";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import Cookies from "js-cookie";
 import { axiosNoAuth } from "../api/Axios";
 import axios from "axios";
 
