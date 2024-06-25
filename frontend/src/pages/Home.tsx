c99b38672ba910a9ec4311ada19868cad9244add
Maximilian Falco Widjaya <maximilianfalco@gmail.com>
2024-06-25 21:14:39 +1000

    removed log out button from home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,8 @@
-import ButtonPrimary from "../components/ButtonPrimary"
-
 const Home = () => {
   return (
     <div className="h-screen flex">
       <div className="w-96 max-h-40 flex flex-wrap p-2 rounded-md mx-auto my-auto">
         <div className="w-full text-center my-2">You Have Reached the Home Page</div>
-        <ButtonPrimary text="Click here to Log Out!" url="" />
       </div>
     </div>
   )
