51d17691ff34c31481cbd6fd9e469ab14fa21dd9
Edrick Koda <z5349845@unsw.edu.au>
2024-06-25 23:09:51 +1000

    implemented loading for all buttons

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -9,6 +9,8 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { axiosNoAuth } from "../api/Axios";
 import axios from "axios";
+import { useState } from "react";
+import ButtonLoading from "../components/ButtonLoading";
 
 type LoginProps = z.infer<typeof loginSchema>;
 
@@ -16,25 +18,33 @@ axios.defaults.withCredentials = true;
 
 export default function Landing() {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
-  const { register, handleSubmit, formState: { errors }} = useForm<LoginProps>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginProps>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
       email: "",
       password: "",
-    }
-  })
+    },
+  });
 
   const onSubmit = async (data: LoginProps) => {
     try {
-      await axiosNoAuth.post("/auth/login",
-        { email: data.email, password: data.password}
-      );
+      setLoading(true);
+      await axiosNoAuth.post("/auth/login", {
+        email: data.email,
+        password: data.password,
+      });
       navigate("/dashboard");
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+    setLoading(false);
+  };
 
   return (
     <div className="min-h-screen min-w-screen flex">
@@ -45,39 +55,66 @@ export default function Landing() {
             Skill Issue
           </h2>
           <p className="text-xl text-gray-600 text-center">Welcome back!</p>
-          
+
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className="mt-8">
               <label className="block text-gray-700 text-sm mb-2">
                 Email Address
               </label>
-              <Textbox id="email" {...register("email")} name="email" type="email" autoComplete="email" />
-              {errors.email && <p className="text-red-600 text-sm">{errors.email.message}</p>}
+              <Textbox
+                id="email"
+                {...register("email")}
+                name="email"
+                type="email"
+                autoComplete="email"
+              />
+              {errors.email && (
+                <p className="text-red-600 text-sm">{errors.email.message}</p>
+              )}
             </div>
             <div className="mt-8">
               <div className="flex justify-between">
-                <label className="block text-gray-700 text-sm mb-2">Password</label>
-                <a href="#" className="text-xs font-normal text-indigo-600 hover:underline h-fit">
+                <label className="block text-gray-700 text-sm mb-2">
+                  Password
+                </label>
+                <a
+                  href="#"
+                  className="text-xs font-normal text-indigo-600 hover:underline h-fit"
+                >
                   Forgot your password?
                 </a>
               </div>
-              <Textbox id="password" {...register("password")} name="password" type="password" autoComplete="password" />
-              {errors.password && <p className="text-red-600 text-sm">{errors.password.message}</p>}
+              <Textbox
+                id="password"
+                {...register("password")}
+                name="password"
+                type="password"
+                autoComplete="password"
+              />
+              {errors.password && (
+                <p className="text-red-600 text-sm">
+                  {errors.password.message}
+                </p>
+              )}
             </div>
 
             <div className="mt-8" title="Sign-In">
               {/* <ButtonPrimary text="Sign-In" url="/dashboard" /> */}
-              <ButtonSubmit text="Sign-In" />
+              {loading ? 
+                <ButtonLoading/> : <ButtonSubmit text="Sign-In" />}
             </div>
           </form>
-          
+
           <div className="mt-4 flex gap-1 items-center justify-center">
-            <Link to="/register" className="text-xs text-indigo-600 hover:text-indigo-300 font-bold">
+            <Link
+              to="/register"
+              className="text-xs text-indigo-600 hover:text-indigo-300 font-bold"
+            >
               Don't Have an Account? Register Here!
             </Link>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
