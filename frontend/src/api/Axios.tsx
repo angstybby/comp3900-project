547bd649adce767bc9965ff98064ece5c9a7b083
Maximilian Falco Widjaya <maximilianfalco@gmail.com>
2024-06-25 13:53:56 +1000

    updated axios instance names and readme

--- frontend/src/api/Axios.tsx
+++ frontend/src/api/Axios.tsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const axiosInstance = axios.create({
+const axiosInstanceWithAuth = axios.create({
   baseURL: 'https://localhost:5005',
 });
 
-axiosInstance.interceptors.request.use((config) => {
+axiosInstanceWithAuth.interceptors.request.use((config) => {
   const token = getToken();
   console.log(token)
   config.headers.Authorization = `Bearer ${token}`;
@@ -17,4 +17,4 @@ const getToken = () => {
   return Cookies.get('token');
 }
 
-export { axiosInstance }
+export { axiosInstanceWithAuth }
