29d4318ec52613b3065abb7bb584174e070a5700
Maximilian Falco Widjaya <maximilianfalco@gmail.com>
2024-06-25 21:35:44 +1000

    added admin page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div className="h-screen flex">
       <div className="w-96 max-h-40 flex flex-wrap p-2 rounded-md mx-auto my-auto">
-        <div className="w-full text-center my-2">You Have Reached the Home Page</div>
+        <div className="w-full text-center my-2">You Have Reached the Home (Dashboard) Page</div>
       </div>
     </div>
   )
