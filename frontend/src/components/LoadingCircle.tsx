4ab33abc8384a8b5f9110d303f3d961e6c2a6386
Maximilian Falco Widjaya <maximilianfalco@gmail.com>
2024-07-07 02:45:32 +1000

    fixed small alignment issues

--- frontend/src/components/LoadingCircle.tsx
+++ frontend/src/components/LoadingCircle.tsx
@@ -1,6 +1,6 @@
 const LoadingCircle = () => {
   return (
-    <svg className="animate-spin h-5 w-5 mr-3 " viewBox="0 0 24 24">
+    <svg className="animate-spin h-5 w-5" viewBox="0 0 24 24">
       <circle
         className="opacity-25"
         cx="12"
