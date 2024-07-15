bb0dd7ed405d8f45775e9ad1191a90f5c0d8141b
Jovanka <dev.kniamaps@outlook.com>
2024-07-15 19:06:35 +1000

    added stubs for skill gap analysis chart

--- frontend/src/components/ProjectsComponents/SkillsGapAnalysis.tsx
+++ frontend/src/components/ProjectsComponents/SkillsGapAnalysis.tsx
@@ -1,14 +1,55 @@
+import { useEffect, useState } from "react";
+
 export default function SkillsGapAnalysis() {
-  return (
-      <div className="h-32 bg-gray-200 p-5 py-3 text-center rounded-lg">
-        <div className="text-start flex flex-col gap-1">
-            <div className="flex flex-row justify-between items-center">
-                <h1 className="text-2xl font-light">Your Skill Gap Analysis</h1>
+
+    const [projectSkills, setProjectSkills] = useState<string[]>([]);
+    const [studentSkills, setStudentSkills] = useState<string[]>([]);
+    const [skillsGap, setSkillsGap] = useState<string[]>([]);
+
+    //stubs
+    const projectSkillsStub = [
+        'JavaScript',
+        'React',
+        'Node.js',
+        'SQL'
+    ];
+
+    const studentSkillsStub = [
+        'JavaScript',
+        'HTML',
+        'CSS'
+    ];
+
+    useEffect(() => {
+        setProjectSkills(projectSkillsStub);
+        setStudentSkills(studentSkillsStub);
+    }, []);
+
+    useEffect(() => {
+        const gap = projectSkills.filter(
+            (skill) => !studentSkills.includes(skill)
+        );
+        setSkillsGap(gap);
+    }, [projectSkills, studentSkills]);
+
+
+    // useEffect(() => {
+
+    // }, []);
+
+    return (
+        <div className="h-32 bg-gray-200 p-5 py-3 text-center rounded-lg">
+            <div className="text-start flex flex-col gap-1">
+                <div className="flex flex-row justify-between items-center">
+                    <h1 className="text-2xl font-light">Your Skill Gap Analysis</h1>
+                </div>
+                <ul>
+                    {skillsGap.map((skill) => (
+                      <li key={skill}>{skill}</li>
+                    ))}
+                </ul>
             </div>
-            <h2 className="text-sm font-light line-clamp-2">graphs and stuff here</h2>
-            {/* <div className="py-6" id="donut-chart"></div> */}
         </div>
-      </div>
-  );
+    );
 
 }
