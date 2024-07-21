b58afe65bb0175be27f6eb16f2fd826d980cb554
Edrick Koda <z5349845@unsw.edu.au>
2024-07-21 19:07:28 +1000

    fixed a mini bug

--- frontend/src/components/ProjectsComponents/SkillsGapAnalysis.tsx
+++ frontend/src/components/ProjectsComponents/SkillsGapAnalysis.tsx
@@ -75,46 +75,52 @@ export default function SkillsGapAnalysis({
 
   return (
     <div>
+      <div>
+        <h1>Please choose a group</h1>
+        <ProjectGroupOptions chooseOptions={chooseOptions} />
+      </div>
       {groupId == -1 ? (
-        <div>
-          <h1>Please choose a group</h1>
-          <ProjectGroupOptions chooseOptions={chooseOptions} />
-        </div>
+        <></>
       ) : (
-        <p></p>
+        <div>
+          <h1 className="text-xl font-semibold pt-4">
+            Your skills analysis for this project
+          </h1>
+          <div className="mt-8 flex">
+            <canvas ref={chartRef} />
+          </div>
+          <div className="flex mt-8">
+            <div className="mr-4">
+              <h2 className="font-semibold">Skills Required:</h2>
+              <ul>
+                {projectSkills.map((skill, index) => (
+                  <li key={`project-${skill.id}-${index}`}>
+                    {skill.skillName}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="mr-4">
+              <h2 className="font-semibold">Skills Possessed:</h2>
+              <ul>
+                {studentSkills.map((skill, index) => (
+                  <li key={`student-${skill.id}-${index}`}>
+                    {skill.skillName}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="mr-4">
+              <h2 className="font-semibold">Skills Lacking:</h2>
+              <ul>
+                {skillsGap.map((skill, index) => (
+                  <li key={`gap-${skill.id}-${index}`}>{skill.skillName}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
       )}
-      <h1 className="text-xl font-semibold pt-4">
-        Your skills analysis for this project
-      </h1>
-      <div className="mt-8 flex">
-        <canvas ref={chartRef} />
-      </div>
-      <div className="flex mt-8">
-        <div className="mr-4">
-          <h2 className="font-semibold">Skills Required:</h2>
-          <ul>
-            {projectSkills.map((skill, index) => (
-              <li key={`project-${skill.id}-${index}`}>{skill.skillName}</li>
-            ))}
-          </ul>
-        </div>
-        <div className="mr-4">
-          <h2 className="font-semibold">Skills Possessed:</h2>
-          <ul>
-            {studentSkills.map((skill, index) => (
-              <li key={`student-${skill.id}-${index}`}>{skill.skillName}</li>
-            ))}
-          </ul>
-        </div>
-        <div className="mr-4">
-          <h2 className="font-semibold">Skills Lacking:</h2>
-          <ul>
-            {skillsGap.map((skill, index) => (
-              <li key={`gap-${skill.id}-${index}`}>{skill.skillName}</li>
-            ))}
-          </ul>
-        </div>
-      </div>
     </div>
   );
 }
