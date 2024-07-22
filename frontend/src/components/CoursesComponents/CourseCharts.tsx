f9518bf52ed40463ef7b498af98002ecbe864130
Jovanka <dev.kniamaps@outlook.com>
2024-07-22 16:12:42 +1000

    pushing to pull

--- frontend/src/components/CoursesComponents/CourseCharts.tsx
+++ frontend/src/components/CoursesComponents/CourseCharts.tsx
@@ -1,5 +1,8 @@
 import { Radar } from 'react-chartjs-2';
 import { Chart as ChartJS, Tooltip, Legend, RadialLinearScale, LineElement, PointElement, Filler } from 'chart.js';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { axiosInstanceWithAuth } from '@/api/Axios';
 
 
 ChartJS.register(
@@ -13,25 +16,34 @@ ChartJS.register(
 const CourseCharts: React.FC = () => {
 
   //still stubs
-  const skillLabels = ['Javascript', 'Software design', 'Software testing', 'Object-Oriented', 'Most important Skill'];
-  const skillLevels = ['3', '2', '5', '4', '3'];
-  const skillImportance = ['1', '2', '4', '3', '5'];
+  // const skillLabels = ['Javascript', 'Software design', 'Software testing', 'Object-Oriented', 'Most important Skill'];
+  // const skillLevels = ['3', '2', '5', '4', '3'];
+  // const skillImportance = ['1', '2', '4', '3', '5'];
   // const popularity = 50;
 
 
+  const [skillsData, setSkillsData] = useState<{skillName: string; popularity: number;}[]>([]);
+ 
+  useEffect(() => {
+    const fetchSkillsPopularity = async () => {
+      try {
+        const response = await axiosInstanceWithAuth.get('/skills/popularity');
+        setSkillsData(response.data);
+      } catch (error) {
+        console.error('Error fetching skills popularity:', error)
+      }
+    };
+
+    fetchSkillsPopularity();
+  }, []);
 
   const radarData = {
-    labels: skillLabels,
+    labels: skillsData.map(skill => skill.skillName),
     datasets: [
       {
-        label: 'Skill Level',
-        data: skillLevels,
+        label: 'Skill Popularity',
+        data: skillsData.map(skill => skill.popularity),
         backgroundColor: 'rgba(54, 162, 235, 0.5)',
-      },
-      {
-        label: 'Skill Importance',
-        data: skillImportance,
-        borderColor: 'rgb(255, 99, 132)',
       },
       
     ],
