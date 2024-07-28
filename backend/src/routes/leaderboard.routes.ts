dcd1b232bb7e37f315a783cb7961b29b1d20db20
Edrick Koda <z5349845@unsw.edu.au>
2024-07-28 16:34:42 +1000

    iumplemented the front end and conencted it to the backend

--- backend/src/routes/leaderboard.routes.ts
+++ backend/src/routes/leaderboard.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import {
-    dbGetRankingBySkip,
+    dbGetAllRankings,
     dbGetRankingByZid,
 } from "../models/leaderboard.models";
 import { CustomRequest } from "../middleware/auth.middleware";
@@ -8,29 +8,19 @@ import { CustomRequest } from "../middleware/auth.middleware";
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-    const { skip, search } = req.body;
-
     try {
-        const response = await dbGetRankingBySkip(skip, search);
-        console.log("Leaderboard response:", response);
-        res.status(200).send(response);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error);
-    }
-});
-
-router.get("/ranking", async (req, res) => {
-    const customReq = req as CustomRequest;
-    if (!customReq.token || typeof customReq.token === "string") {
-        return res.status(401).send("Unauthorized");
-    }
-
-    const zid = customReq.token.zid;
-
-    try {
-        const response = await dbGetRankingByZid(zid);
-        res.status(200).send(response);
+        const response = await dbGetAllRankings();
+        // format response
+        const formattedResponse = response.map((profile, index) => {
+            return {
+                rank: index + 1,
+                zid: profile.zid,
+                fullname: profile.fullname,
+                skillCount: profile._count.Skills,
+            };
+        });
+        console.log("Leaderboard response:", formattedResponse);
+        res.status(200).send(formattedResponse);
     } catch (error) {
         console.error(error);
         res.status(500).send(error);
--- frontend/src/pages/Leaderboard.tsx
+++ frontend/src/pages/Leaderboard.tsx
@@ -1,12 +1,40 @@
+import { axiosInstanceWithAuth } from "@/api/Axios";
 import {
   DataTable,
   LeaderboardColumns,
+  LeaderboardTableProps,
 } from "@/components/LeaderboardComponents/LeaderboardTable";
+import LoadingCircle from "@/components/LoadingCircle";
+import { useEffect, useState } from "react";
+import { set } from "zod";
 
 export default function Leaderboard() {
+  const [data, setData] = useState<LeaderboardTableProps[]>([]);
+  // TODO implement loading state
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axiosInstanceWithAuth.post("/leaderboard");
+        setData(response.data);
+      } catch (error) {
+        console.error(error);
+        //TODO Show the error
+      }
+    };
+    setLoading(true);
+    fetchData();
+    setLoading(false);
+  }, []);
+
   return (
     <div className="container mx-auto py-10">
-      <DataTable columns={LeaderboardColumns} data={[]} />
+      {loading ? (
+        <LoadingCircle />
+      ) : (
+        <DataTable columns={LeaderboardColumns} data={data} />
+      )}
     </div>
   );
 }
