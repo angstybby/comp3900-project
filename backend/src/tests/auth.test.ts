55ba64a7541dc3c7c6fe2966e5d94cc9ca1410a0
Edrick Koda <z5349845@unsw.edu.au>
2024-07-29 23:24:35 +1000

    fully implemented register

--- backend/src/tests/auth.test.ts
+++ backend/src/tests/auth.test.ts
@@ -7,57 +7,47 @@ import { UserType } from "@prisma/client";
 import { sha256 } from "js-sha256";
 
 describe("/auth", async () => {
+    const studentOneZid = "z1111111";
+    const studentOneEmail = "student@gmail.com";
+    const studentOnePassword = "P@ssw0rd";
+    const studentOneFullname = "Student";
+    const studentUserType = UserType.student;
+
+    const studentTwoZid = "z2222222";
+    const studentTwoEmail = "student2@gmail.com";
+    const studentTwoPassword = "P@ssw0rd";
+    const studentFullName = "Student Two";
+
+    const academicOneZid = "z3333333";
+    const academicOneEmail = "academic@gmail.com";
+    const academicOnePassword = "P@ssw0rd";
+    const academicOneFullname = "Academic";
+    const academicUserType = UserType.academic;
+
     describe("[POST] /auth/register", () => {
-        // ----------------------------------------------
-        //              Refrence Test
-        // ----------------------------------------------
-        // it('should respond with a `200` status code and user details', async () => {
-        //   const { status, body } = await request(app).post('/auth/signup').send({
-        //     username: 'testusername',
-        //     password: 'testpassword'
-        //   })
-        //   // 2
-        //  const newUser = await prisma.user.findFirst()
-        //   // 3
-        //  expect(status).toBe(200)
-        //   // 4
-        //  expect(newUser).not.toBeNull()
-        //   // 5
-        //  expect(body.user).toStrictEqual({
-        //     username: 'testusername',
-        //     id: newUser?.id
-        //  })
-        // })
-
-        it("should respond with a `200` status code and user details", async () => {
-            const zid = "z1111111";
-            const email = "student@gmail.com";
-            const password = "P@ssw0rd";
-            const fullname = "Student";
-            const userType = UserType.student;
-
+        it("should respond with a `200` status code and JWT token", async () => {
             const { status, headers } = await request(app)
                 .post("/api/auth/register")
                 .send({
-                    zid,
-                    email,
-                    password,
-                    fullname,
-                    userType,
+                    zid: studentOneZid,
+                    email: studentOneEmail,
+                    password: studentOnePassword,
+                    fullname: studentOneFullname,
+                    userType: studentUserType,
                 });
 
             const newUser = await prisma.user.findFirst({
                 where: {
-                    zid,
-                    email,
-                    userType,
+                    zid: studentOneZid,
+                    email: studentOneEmail,
+                    userType: studentUserType,
                 },
             });
 
             const newProfile = await prisma.profile.findFirst({
                 where: {
-                    zid,
-                    fullname,
+                    zid: studentOneZid,
+                    fullname: studentOneFullname,
                 },
             });
 
@@ -65,11 +55,108 @@ describe("/auth", async () => {
             // Do not need to check as it should already equal by search query
             expect(newUser).not.toBeNull();
             expect(newProfile).not.toBeNull();
-            expect(newUser?.password === sha256(password));
+            expect(newUser?.password === sha256(studentOnePassword));
 
-            const cookies = headers['set-cookie'];
+            const cookies = headers["set-cookie"];
             expect(cookies).toBeDefined();
-            
+        });
+
+        it("should respons with `200` status code and JWT token for academic", async () => {
+            const { status, headers } = await request(app)
+                .post("/api/auth/register")
+                .send({
+                    zid: academicOneZid,
+                    email: academicOneEmail,
+                    password: academicOnePassword,
+                    fullname: academicOneFullname,
+                    userType: academicUserType,
+                });
+
+            const newUser = await prisma.user.findFirst({
+                where: {
+                    zid: academicOneZid,
+                    email: academicOneEmail,
+                    userType: academicUserType,
+                },
+            });
+
+            const newProfile = await prisma.profile.findFirst({
+                where: {
+                    zid: academicOneZid,
+                    fullname: academicOneFullname,
+                },
+            });
+
+            expect(status).toBe(200);
+            // Do not need to check as it should already equal by search query
+            expect(newUser).not.toBeNull();
+            expect(newProfile).not.toBeNull();
+            expect(newUser?.password === sha256(studentOnePassword));
+
+            const cookies = headers["set-cookie"];
+            expect(cookies).toBeDefined();
+        });
+
+        it("should respond with a `409` status code if email or zid is already in use", async () => {
+            const responseOne = await request(app)
+                .post("/api/auth/register")
+                .send({
+                    zid: studentOneZid,
+                    email: studentOneEmail,
+                    password: studentOnePassword,
+                    fullname: studentOneFullname,
+                    userType: studentUserType,
+                });
+            expect(responseOne.status).toBe(200);
+
+            const responseTwo = await request(app)
+                .post("/api/auth/register")
+                .send({
+                    zid: studentOneZid,
+                    email: studentTwoEmail,
+                    password: studentOnePassword,
+                    fullname: studentOneFullname,
+                    userType: studentUserType,
+                });
+
+            expect(responseTwo.status).toBe(409);
+
+            const responseThree = await request(app)
+                .post("/api/auth/register")
+                .send({
+                    zid: studentTwoZid,
+                    email: studentOneEmail,
+                    password: studentOnePassword,
+                    fullname: studentOneFullname,
+                    userType: studentUserType,
+                });
+
+            expect(responseThree.status).toBe(409);
+        });
+
+        it("should respond with a `400` status code if some details are missing", async () => {
+            // Missing userType
+            const responseOne = await request(app)
+                .post("/api/auth/register")
+                .send({
+                    zid: studentOneZid,
+                    email: studentOneEmail,
+                    password: studentOnePassword,
+                    fullname: studentOneFullname,
+                });
+
+            expect(responseOne.status).toBe(400);
+
+            const responseTwo = await request(app)
+                .post("/api/auth/register")
+                .send({
+                    email: studentOneEmail,
+                    password: studentOnePassword,
+                    fullname: studentOneFullname,
+                    userType: studentUserType,
+                });
+
+            expect(responseTwo.status).toBe(400);
         });
     });
 });
