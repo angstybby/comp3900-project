3ada7cd9b8c8cc0819fdc9c4e0298dc0c3fd7149
Maximilian Falco Widjaya <maximilianfalco@gmail.com>
2024-07-31 00:54:34 +1000

    added some extra docs

--- backend/src/routes/group.routes.ts
+++ backend/src/routes/group.routes.ts
@@ -770,7 +770,13 @@ router.post("/get-group-reco", authMiddleWare, async (req, res) => {
   }
 });
 
-
+/**
+ * @route POST /group/get-group-reco
+ * @desc Get recommended groups based on skills and description
+ * @access Private
+ * @returns {Array} List of recommended groups
+ * @throws {500} If an error occurs while fetching recommended groups
+ */
 const createUserSkillsString = async (zid: string) => {
   try {
       const user = await dbGetUserSkills(zid);
@@ -788,7 +794,19 @@ const createUserSkillsString = async (zid: string) => {
   }
 };
 
-
+/**
+ * @route POST /group/feedback
+ * @desc Create a new feedback
+ * @access Private
+ * @type RequestHandler
+ * @param {string} fromZid - The zid of the user giving feedback
+ * @param {string} toZid - The zid of the user receiving feedback
+ * @param {string} comment - The comment of the feedback
+ * @param {number} rating - The rating of the feedback
+ * @returns {Feedback} - The newly created feedback
+ * @throws {Error} - If the request body is missing required fields
+ * @throws {500} - If an error occurs while creating new feedback
+ */
 router.post("/feedback", async (req, res) => {
     const { fromZid, toZid, comment, rating } = req.body;
     
